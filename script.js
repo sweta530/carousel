6eacade161920762ce05e67c79d805bdc1172564
sweta530 <sweta.gohel53@gmail.com>
2024-03-07 15:09:52 +0530

    fix sliding effect

--- script.js
+++ script.js
@@ -3,29 +3,33 @@ document.addEventListener("DOMContentLoaded", function() {
     const carouselItems = document.querySelectorAll(".carousel-item");
     var indicators = document.querySelectorAll('.carousel-number li');
     let currentIndex = 0;
-    let translateX = 0;
 
-    function showSlide(index) {
+    function showSlide(index,currentImageStyle,backImageStyle) {
         if (index >= 0 && index < carouselItems.length) {
-            translateX = -index * 100;
-            carousel.style.transform = `translateX(${translateX}%)`;
-            currentIndex = index;
+
+            carouselItems.forEach(item => {
+                item.classList.remove('active', 'active-left', 'active-right', 'active-back-left', 'active-back-right');
+            });
+            carouselItems[currentIndex].classList.add('active',backImageStyle);
             
+            carouselItems[index].classList.add("active",currentImageStyle);
+           
             indicators.forEach(indicator => {
                 indicator.classList.remove('active');
             })
             indicators[index].classList.add('active');
+            currentIndex = index
         }
     }
 
     function nextSlide() {
-        currentIndex = (currentIndex + 1) % carouselItems.length;
-        showSlide(currentIndex);
+        let index = (currentIndex + 1) % carouselItems.length;
+        showSlide(index,"active-left",'active-back-right');
     }
 
     function previousSlide() {
-        currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
-        showSlide(currentIndex);
+        let index = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+        showSlide(index,"active-right",'active-back-left');
     }
 
     document.getElementById('nextBtn').addEventListener('click', nextSlide);
@@ -33,12 +37,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
     indicators.forEach((indicator, index) => {
         indicator.addEventListener('click', function() {
-            currentIndex = index;
-            showSlide(currentIndex);
+            if (currentIndex < index) {
+                showSlide(index,"active-left",'active-back-right');
+            } else {
+                showSlide(index,"active-right",'active-back-left');
+            }
             indicator.classList.add('active');
         });
     });
 
     // Show the initial slide
-    showSlide(currentIndex);
+    // showSlide(currentIndex,"active");
 });
